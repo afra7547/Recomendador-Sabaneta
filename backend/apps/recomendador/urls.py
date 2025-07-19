c52cd1705d48fbbdb4e3b26508366bc7d711df34
afra7547 <andresrodriguez7547@hotmail.com>
2025-07-19 12:41:12 -0500

    Delete backend directory

--- backend/apps/recomendador/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='recomendador_home'),  # ← Este cambio es clave
-    path('registro/', views.registrar_comercio, name='registro'),
-]
